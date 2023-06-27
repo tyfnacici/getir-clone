237c97b8eae91f6511e83a8632daeb01c648cc13
Tayfun Açıcı <tyfnacici@gmail.com>
2023-06-27 12:40:01 +0300

    fix(footer): disable eslint

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+/* eslint-disable */
 import appstore from "../images/footer/appstore.svg"
 import googleplay from "../images/footer/googleplay.svg"
 import appgalery from "../images/footer/appgallery.svg"
